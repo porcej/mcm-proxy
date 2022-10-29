05599815a2780099c9bf841c897c08fdb0ed4b3d
Joe Porcelli <porcej@gmail.com>
2022-10-29 17:53:23 -0400

    Added self hosting script

---
+++ selfhost_site.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+
+sudo systemctl stop nginx
+sudo nginx -s stop
+
+sudo cp config/mcm-prd-local /etc/nginx/sites-available/mcm-prd-local
+
+sudo rm -rf /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/mcm-prd-local /etc/nginx/sites-enabled/mcm-prd-local
+
+
+python scrape.py
+
+sudo rm -rf /etc/nginx/html/mcm-proxy
+sudo mkdir /etc/nginx/html/mcm-proxy
+
+sudo cp -r tmp/* /etc/nginx/html/mcm-proxy
